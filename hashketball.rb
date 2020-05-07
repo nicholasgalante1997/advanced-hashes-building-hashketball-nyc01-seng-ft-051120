73b9ef6c70323e0ff323af096715a64ddd2f43c0
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-07 15:21:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,38 @@
-# Write your code here!
+def game_hash 
+  hash = {
+    :home => {
+      :team_name => "team name",
+      :colors => ["color one", "color two"],
+      :players => [
+        {
+          :player_name => " ",
+          :number => " ",
+          :shoe => " ",
+          :points => " ", 
+          :rebounds => " ",
+          :assists => " ",
+          :steals => " ",
+          :blocks => " ",
+          :slam_dunks => " "
+        }
+        ]
+    },
+    :away => {
+      :team_name => "team name",
+      :colors => ["color one", "color two"],
+      :players => [
+        {
+          :player_name => " ", 
+          :number => " ", 
+          :shoe => " ", 
+          :points => " ", 
+          :rebounds => " ", 
+          :assists => " ", 
+          :steals => " ", 
+          :blocks => " ", 
+          :slam_dunks => " " 
+        }
+        ]
+    }
+  }
+end 
